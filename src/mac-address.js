fa3641de7f30277ff208b98258e98809fb811659
victoria <v.suprun1961@gmail.com>
2023-12-25 06:45:52 +0200

    feat: complete task

--- src/mac-address.js
+++ src/mac-address.js
@@ -6,6 +6,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * Your task is to check by given string inputString
  * whether it's a MAC-48 address or not.
+ * 
+ * Адрес MAC-48 состоит из шести групп по две шестнадцатеричные цифры (от 0 до 9 или от A до F).
+  * через дефис.
+  *
+  * Ваша задача — проверить по заданной строке inputString
+  * независимо от того, является ли это адрес MAC-48 или нет.
  *
  * @param {Number} inputString
  * @return {Boolean}
@@ -14,9 +20,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const res = (/^([0-9A-Fa-f]{2}-)([0-9A-Fa-f]{2}-)([0-9A-Fa-f]{2}-)([0-9A-Fa-f]{2}-)([0-9A-Fa-f]{2}-)[0-9A-Fa-f]{2}$/).test(n);
+console.log(res);
+return res;
 }
 module.exports = {
   isMAC48Address
