2fe2f2be6e52b52932d10186167c48cb5a114b52
victoria <v.suprun1961@gmail.com>
2023-12-26 01:42:06 +0200

    feat: complete task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -23,9 +23,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+
+  domains.forEach((domain) => {
+    const changeDomains = domain.split(".").reverse();
+    let currentchangeDomains = "";
+
+    changeDomains.forEach((changeDomain) => {
+      currentchangeDomains += `.${changeDomain}`;
+      result[currentchangeDomains] = (result[currentchangeDomains] || 0) + 1;
+    });
+  });
+
+  return result;
 }
 
 module.exports = {
