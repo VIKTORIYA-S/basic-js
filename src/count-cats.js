cd860c68764a857be16d33479a23ba1b821b1d63
victoria <v.suprun1961@gmail.com>
2023-12-26 00:24:15 +0200

    feat: complete task

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,16 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  let quantityCats = 0;
-    matrix.forEach(array => {
-      array.forEach(item => {
-        if (item === '^^') {
-          quantityCats ++;
-          console.log(quantityCats)
-        }
-      })
-    })
+  if (matrix.length === 0) {
+    return 0;
   }
+
+  const result = matrix.map((element) => count(element)).reduce((a, b) => a + b);
+  return result;
+}
+
+function count(array) {
+  const result = array.filter((element) => element === '^^').length;
+  return result;
+}
+
 
 module.exports = {
   countCats
