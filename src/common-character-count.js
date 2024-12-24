4737c84a2ec67791ede8feb7354e81fc99fb3252
victoria <v.suprun1961@gmail.com>
2024-12-25 00:03:37 +0200

    feat: complete task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,12 +12,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const frequency1 = {};
+  const frequency2 = {};
+  let commonCount = 0;
+
+  for (const char of s1) {
+    if (frequency1[char]) {
+      frequency1[char]++;
+    } else {
+      frequency1[char] = 1;
+    }
+  }
+
+  for (const char of s2) {
+    if (frequency2[char]) {
+      frequency2[char]++;
+    } else {
+      frequency2[char] = 1;
+    }
+  }
+
+  for (const char in frequency1) {
+    if (frequency2[char]) {
+      commonCount += Math.min(frequency1[char], frequency2[char]);
+    }
+  }
+
+  return commonCount;
   // remove line with error and write your code here
 }
 
 module.exports = {
   getCommonCharacterCount
 };
+
+
+
+
+  // Подсчет частоты появления символов в первой строке
+
+
+  // Подсчет частоты появления символов во второй строке
+
+
+  // Подсчет общих символов
+
+
+// Пример использования
+// const str1 = "apple";
+// const str2 = "peach";
+// console.log(commonCharactersCount(str1, str2));
+// // Вывод: 3 (символы 'a', 'p' и 'e')
+
