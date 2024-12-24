373c3eca14ef19f065f782f9d175aaa46074f1bc
victoria <v.suprun1961@gmail.com>
2024-12-25 00:22:50 +0200

    feat: complete task

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,8 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  return members .map(name => name.charAt(0).toUpperCase()).sort().join('');
   // remove line with error and write your code here
 }
 
