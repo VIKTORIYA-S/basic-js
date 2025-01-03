4c3f3ec89302e211294ed6c1599bcfff4cfb7132
victoria <v.suprun1961@gmail.com>
2025-01-03 03:29:40 +0200

    feat: complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  class VigenereCipheringMachine {
+    constructor(direct = true) {
+        this.direct = direct;
+    }
+
+    encrypt(message, key) {
+        if (!message || !key) throw new Error('Incorrect arguments!');
+        return this.process(message, key, 'encrypt');
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+        return this.process(encryptedMessage, key, 'decrypt');
+    }
+
+    process(input, key, method) {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        input = input.toUpperCase();
+        key = key.toUpperCase();
+        let output = '';
+        let keyIndex = 0;
+
+        for (let i = 0; i < input.length; i++) {
+            const inputChar = input[i];
+            if (alphabet.includes(inputChar)) {
+                const inputIndex = alphabet.indexOf(inputChar);
+                const keyChar = key[keyIndex % key.length];
+                const keyIndexShift = alphabet.indexOf(keyChar);
+
+                let outputIndex;
+                if (method === 'encrypt') {
+                    outputIndex = (inputIndex + keyIndexShift) % alphabet.length;
+                } else {
+                    outputIndex = (inputIndex - keyIndexShift + alphabet.length) % alphabet.length;
+                }
+
+                output += alphabet[outputIndex];
+                keyIndex++;
+            } else {
+                output += inputChar;
+            }
+        }
+
+        if (!this.direct) {
+            output = output.split('').reverse().join('');
+        }
+
+        return output;
+    }
 }
 
 module.exports = {
