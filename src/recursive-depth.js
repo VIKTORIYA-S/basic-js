bb1aeab4e6265434f72e353df88744ca18cbd4e6
victoria <v.suprun1961@gmail.com>
2025-01-03 03:12:44 +0200

    feat: complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,9 +12,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+
+    class DepthCalculator {
+      calculateDepth(arr) {
+        if (!Array.isArray(arr)) return 0; // Проверка, является ли arr массивом
+        let depth = 1; // Начальная глубина
+        for (let item of arr) {
+          if (Array.isArray(item)) {
+            depth = Math.max(depth, this.calculateDepth(item) + 1);
+          }
+        }
+        return depth;
     // remove line with error and write your code here
   }
 }
